4337d18e4448264e2ccdc5ff1d1e3ab9acedc4cf
pruthvi03 <hingu.bloggers@gmail.com>
2021-03-04 15:05:49 +0530

    second commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,12 +7,3 @@ mongoose.connect(process.env.MONGODB_URL,
         useUnifiedTopology: true
     }
 ).then(connect => { console.log('connected to db successfully'); })
-
-
-// const me = new User({ name: '  Sigh Sighaniya  ', email: '   PR2@gmail.com',password:'ecrptedpass123'});
-// me.save().then(() => console.log(me)).catch(err => console.log(err));
-
-
-
-// const myTask = new Task({ completed:true});
-// myTask.save().then(() => console.log(myTask)).catch(err => console.log(err));
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,14 +5,10 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token.trim(), process.env.JWT_SECRET);
-        // console.log(token.trim());
-        // console.log(decoded);
         const user = await User.findOne({_id:decoded._id,'tokens.token':token.trim()});
-        // console.log(user)
         if (!user){throw new Error()}
         req.user = user;
         req.token = token;
-        // console.log(req)
         next();
 
     } catch (error) {
